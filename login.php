bc4b3daecd0054c7b11c6891fea026c56616da38
Alan-RamiMan <alan.ramirez.m@hotmail.com>
2023-11-13 06:00:39 -0300

    Actu

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 error_reporting(0);
 include("dbconnection.php");
-if (isset($_POST['login'])) {
+if (isset($_POST['app/login'])) {
   $ret = mysqli_query($con, "SELECT * FROM user WHERE email='" . $_POST['email'] . "' and password='" . $_POST['password'] . "'");
   $num = mysqli_fetch_array($ret);
   if ($num > 0) {
