622ce22046cd0ec8f26cb7dc1eaad862c1114728
Alan-RamiMan <alan.ramirez.m@hotmail.com>
2023-11-14 03:02:06 -0300

    Nuevo Repo4

--- dbconnection.php
+++ dbconnection.php
@@ -1,12 +1,11 @@
 <?php
-$DB_HOST = $_ENV["DB_HOST"];
-$DB_USER = $_ENV["DB_USER"];
-$DB_PASSWORD = $_ENV["DB_PASSWORD"];
-$DB_NAME = $_ENV["DB_NAME"];
-$DB_PORT = $_ENV["DB_PORT"];
+$DB_HOST="viaduct.proxy.rlwy.net"
+$DB_NAME="railway"
+$DB_PASSWORD="gCbeAE16haG5gGE53f1aaCG4cabDAf65"
+$DB_USER="root"
 
 // Create connection
-$conn = mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
+$conn = mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
