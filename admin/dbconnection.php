e763109f75ae151557ca8b7c36c24d417c1cd09f
Alan-RamiMan <alan.ramirez.m@hotmail.com>
2023-11-14 02:52:44 -0300

    Nuevo Repo3

---
+++ admin/dbconnection.php
@@ -0,0 +1,16 @@
+<?php
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
+
+// Create connection
+$conn = mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
