a485d97d5f823c3cb8d372094f598db93f4f9b16
Alan-RamiMan <alan.ramirez.m@hotmail.com>
2023-11-14 04:01:39 -0300

    Nuevo Return

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -1,20 +1,15 @@
 <?php
-session_start();
-// $DB_HOST=$_ENV["DB_HOST"];
-// $DB_USER=$_ENV["DB_USER"];
-// $DB_PASSWORD=$_ENV["DB_PASSWORD"];
-// $DB_NAME=$_ENV["DB_NAME"];
-// $DB_PORT=$_ENV["DB_PORT"];
+// session_start();
+$DB_HOST=$_ENV["DB_HOST"];
+$DB_USER=$_ENV["DB_USER"];
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];
+$DB_NAME=$_ENV["DB_NAME"];
+$DB_PORT=$_ENV["DB_PORT"];
 
 
-DB_HOST=viaduct.proxy.rlwy.net
-DB_USER=railway
-DB_PASSWORD=gCbeAE16haG5gGE53f1aaCG4cabDAf65
-DB_NAME=19716
-DB_PORT=root
 
 // Create connection
-$con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,DB_PORT);
+$con = mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
