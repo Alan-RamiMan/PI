b2ac7a93b17efe049e0ce069e21c5c1d5fcd0297
Alan-RamiMan <alan.ramirez.m@hotmail.com>
2023-11-14 03:59:03 -0300

    Nuevo Repo10222

--- dbconnection.php
+++ dbconnection.php
@@ -7,14 +7,14 @@ session_start();
 // $DB_PORT=$_ENV["DB_PORT"];
 
 
-$DB_HOST=viaduct.proxy.rlwy.net
-$DB_USER=railway
-$DB_PASSWORD=gCbeAE16haG5gGE53f1aaCG4cabDAf65
-$DB_NAME=19716
-$DB_PORT=root
+DB_HOST=viaduct.proxy.rlwy.net
+DB_USER=railway
+DB_PASSWORD=gCbeAE16haG5gGE53f1aaCG4cabDAf65
+DB_NAME=19716
+DB_PORT=root
 
 // Create connection
-$con = mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
+$con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,DB_PORT);
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
