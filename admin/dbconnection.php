dbd1a668c1cacf46f234a7928ceacc1b55f16f34
Alan-RamiMan <alan.ramirez.m@hotmail.com>
2023-11-14 03:33:30 -0300

    Nuevo Repo88

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -12,6 +12,6 @@ $con = mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT"
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 mysqli_close($con);
 ?>
